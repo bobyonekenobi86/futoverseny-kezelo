33bbf78fccfa9a5fa71978b985db792691f0dfa6
bobyonekenobi86 <attila.bobek@protonmail.com>
2024-12-22 10:44:18 +0100

    Initial commit: Spring Boot projekt létrehozása

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # futoverseny-kezelo
 Alkalmazásfejlesztés beadandó
+
+GDE beadandó feladat, ami egy webalkalmazás, amely képes kezelni egy futóverseny résztvevőinek adatait, eredményeit, és megjeleníteni azok statisztikáit.
---
+++ src/test/java/com/example/futoverseny/FutoversenyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.futoverseny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutoversenyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
